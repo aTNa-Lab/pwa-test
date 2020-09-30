7cca73f7bfab26b80feaf61192279acaae013cfc
aTNa <atay.narynov@gmail.com>
2020-09-30 16:56:12 +0600

    Updates

---
+++ precache-manifest.099c40e4eaa65098cf94f777350b35f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba6ab5f0bb65f1061a762f40124b9af",
+    "url": "/pwa-test/index.html"
+  },
+  {
+    "revision": "41deb5bc69f4386c10c0",
+    "url": "/pwa-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d0cf01a5366ba8caabf",
+    "url": "/pwa-test/static/js/2.93496c34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pwa-test/static/js/2.93496c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41deb5bc69f4386c10c0",
+    "url": "/pwa-test/static/js/main.b3241a24.chunk.js"
+  },
+  {
+    "revision": "92535314b103e3167ccd",
+    "url": "/pwa-test/static/js/runtime-main.3cb95e32.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
