786b51480eaa20c0b3dba017ba172a8d717051ab
aTNa <atay.narynov@gmail.com>
2020-09-30 19:11:55 +0600

    Updates

--- precache-manifest.8715b358cf536fe14e9224fcbe4aeb15.js
+++ precache-manifest.8715b358cf536fe14e9224fcbe4aeb15.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5d13ca202f9a4e2e56f39673bfd5a5d6",
+    "revision": "fa36ca6d285f278b64da8df0e4ab2333",
     "url": "/pwa-test/index.html"
   },
   {
-    "revision": "9c4bb0360f8bdf1a3c39",
+    "revision": "c3a518247875ed4cb704",
     "url": "/pwa-test/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "7d0cf01a5366ba8caabf",
-    "url": "/pwa-test/static/js/2.93496c34.chunk.js"
+    "revision": "ef3a6c82f3dd0ad9bc4b",
+    "url": "/pwa-test/static/js/2.b3bc8c00.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/pwa-test/static/js/2.93496c34.chunk.js.LICENSE.txt"
+    "url": "/pwa-test/static/js/2.b3bc8c00.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9c4bb0360f8bdf1a3c39",
-    "url": "/pwa-test/static/js/main.84a61d9d.chunk.js"
+    "revision": "c3a518247875ed4cb704",
+    "url": "/pwa-test/static/js/main.f584e0c4.chunk.js"
   },
   {
     "revision": "92535314b103e3167ccd",
